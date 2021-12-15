480139c85e3f1e8a8846bb73428c1360eeae80d6
Matheus Cruz <mlcruz@inf.ufrgs.br>
2021-12-15 14:27:52 -0300

    add scan_match_count command

--- src/commands.rs
+++ src/commands.rs
@@ -81,6 +81,14 @@ macro_rules! implement_commands {
                 c.iter(self)
             }
 
+            /// Incrementally iterate the keys space for keys matching a pattern with some count.
+            #[inline]
+            fn scan_match_count<P: ToRedisArgs, RV: FromRedisValue>(&mut self, pattern: P, count: usize) -> RedisResult<Iter<'_, RV>> {
+                let mut c = cmd("SCAN");
+                c.cursor_arg(0).arg("MATCH").arg(pattern).arg("COUNT").arg(count);
+                c.iter(self)
+            }
+
             /// Incrementally iterate hash fields and associated values.
             #[inline]
             fn hscan<K: ToRedisArgs, RV: FromRedisValue>(&mut self, key: K) -> RedisResult<Iter<'_, RV>> {
